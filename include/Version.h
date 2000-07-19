8ddd6d3eee0711078bb53849fd83e9d368c94874
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2000-07-19 08:39:07 +0000

    *** empty log message ***

--- include/Version.h
+++ include/Version.h
@@ -1,3 +1,3 @@
 #define GETDP_VERSION 0.75
-#define GETDP_BUILD "Fri Jul 14 10:19:18 MET DST 2000 on elap57.montefiore.ulg.ac.be (geuzaine)"
+#define GETDP_BUILD "Wed Jul 19 09:53:06 MET DST 2000 on elap57.montefiore.ulg.ac.be (geuzaine)"
 #define GETDP_OS "OSF1 V4.0"
