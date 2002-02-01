e86e6c5681ea27234470302381a3541f4027c41e
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2002-02-01 17:25:26 +0000

    Third kind spherical Bessel functions (aka Hankel functions)

--- Numeric/SphBessel.c
+++ Numeric/SphBessel.c
@@ -1,4 +1,4 @@
-#define RCSID "$Id: SphBessel.c,v 1.4 2002-01-19 01:07:21 geuzaine Exp $"
+#define RCSID "$Id: SphBessel.c,v 1.5 2002-02-01 17:25:26 geuzaine Exp $"
 
 #include <stdio.h>
 #include <math.h>
@@ -16,7 +16,6 @@
 /*  SPHERICAL BESSEL functions of the FIRST kind                            */
 /* ------------------------------------------------------------------------ */
 
-
 double Spherical_j_n(int n, double x){
   /* Computes the Spherical Bessel function 'j' of order n and Real argument */
   
@@ -29,18 +28,17 @@ double Spherical_j_n(int n, double x){
     jsph = (n == 0) ? 1. : 0.; 
   else{
     fnu = n + 0.5;
-    zbesj_( &x, &xi, &fnu, &kode, &NB, &bjr, &bji, &nz, &ierr) ;
+    zbesj_(&x, &xi, &fnu, &kode, &NB, &bjr, &bji, &nz, &ierr) ;
+    if(ierr) Msg(ERROR, "Exception caught in first kind Bessel function");
     jsph = sqrt(0.5*PI/x) * bjr;
   }
   
   GetDP_Return(jsph);  
 }
 
-
-
 void Spherical_j_nArray(int n, double x, int NB, double jsph[]){
   /* Computes NB Spherical Bessel function 'j' Real argument x
-   n is the order of the first member of the sequence */
+     n is the order of the first member of the sequence */
 
   double cte, fnu, xi = 0., jsphi[NBR_MAX_EXP] ;
   int i, nz = 0, ierr, kode = 1 ;
@@ -48,7 +46,8 @@ void Spherical_j_nArray(int n, double x, int NB, double jsph[]){
   GetDP_Begin("Spherical_j_n_Array");
 
   if (NB > NBR_MAX_EXP)
-    Msg(ERROR, "The number of Spherical_j_n required exceeds the maximum defined: NBR_MAX_EXP = %d",NBR_MAX_EXP) ;
+    Msg(ERROR, "The number of Spherical_j_n required exceeds the maximum "
+	"defined: NBR_MAX_EXP = %d",NBR_MAX_EXP) ;
  
  for(i = 0 ; i < NB ; i++ )
     jsph[i] = 0.; 
@@ -58,7 +57,8 @@ void Spherical_j_nArray(int n, double x, int NB, double jsph[]){
   }
   else{
     fnu = n + 0.5 ; /* From order n to (n+NB-1) */
-    zbesj_( &x, &xi, &fnu, &kode, &NB, jsph, jsphi, &nz, &ierr) ;
+    zbesj_(&x, &xi, &fnu, &kode, &NB, jsph, jsphi, &nz, &ierr) ;
+    if(ierr) Msg(ERROR, "Exception caught in first kind Bessel function");
     cte = sqrt(0.5*PI/x);    
     for(i = 0 ; i < NB ; i++ ) jsph[i] *= cte ;
   }
@@ -66,10 +66,9 @@ void Spherical_j_nArray(int n, double x, int NB, double jsph[]){
   GetDP_End ;  
 }
 
-
-
 double AltSpherical_j_n(int n, double x){
-  /* Computes the Alernative Spherical Bessel function 'j' of order n and Real argument */
+  /* Computes the Alernative Spherical Bessel function 'j' of order n
+     and Real argument */
   
   double ajsph, abjr, abji, fnu, xi = 0. ;
   int nz = 0, ierr, kode=1, NB=1 ;
@@ -77,90 +76,12 @@ double AltSpherical_j_n(int n, double x){
   GetDP_Begin("AltSpherical_j_n");
 
   fnu = n + 0.5;
-  zbesj_( &x, &xi, &fnu, &kode, &NB, &abjr, &abji, &nz, &ierr) ;
-
+  zbesj_(&x, &xi, &fnu, &kode, &NB, &abjr, &abji, &nz, &ierr) ;
+  if(ierr) Msg(ERROR, "Exception caught in first kind Bessel function");
   ajsph = sqrt(0.5*PI*x) * abjr;
    
   GetDP_Return(ajsph);
-      
 }
-
-
-
-/* ------------------------------------------------------------------------ */
-/*  SPHERICAL BESSEL functions of the SECOND kind                           */
-/* ------------------------------------------------------------------------ */
-
-double Spherical_y_n(int n, double x){
-  /* Computes the Spherical Bessel function 'y' of order n and real argument */
-  
-  double ysph, byr, byi, auxbyr, auxbyi, fnu, xi = 0. ;
-  int nz = 0,ierr, kode = 1, NB =1 ;
-
-  GetDP_Begin("Spherical_y_n");
-
-  if (x==0.) Msg(ERROR, "Singularity in Spherical_y_n");
-
-  fnu = n+0.5;
-  zbesy_( &x, &xi, &fnu, &kode, &NB, &byr, &byi, &nz, &auxbyr, &auxbyi,
-	  &ierr);
-  
-  ysph = sqrt(0.5*PI/x) * byr;
-
-  GetDP_Return(ysph);   
-}
-
-
-
-void Spherical_y_nArray(int n, double x, int NB, double ysph[]){
-  /* Computes NB Spherical Bessel function 'y' Real argument x
-   n is the order of the first member of the sequence */
-
-  double cte, fnu, xi = 0., ysphi[NBR_MAX_EXP], auxbyr[NBR_MAX_EXP], auxbyi[NBR_MAX_EXP] ;
-  int i, nz = 0, ierr, kode = 1 ;
-
-  GetDP_Begin("Spherical_y_n_Array");
-
-
-  if (NB > NBR_MAX_EXP)
-    Msg(ERROR, "The number of Spherical_y_n required exceeds the maximum defined: NBR_MAX_EXP = %d",NBR_MAX_EXP) ;
-
-  for(i = 0 ; i < NB ; i++ )
-    ysphi[i] = 0.; 
-
-  if (x == 0.){
-    ysph[0] = 1. ;
-  }
-  else{
-    fnu = n + 0.5 ; /* From order n to (n+NB-1) */
-    zbesy_( &x, &xi, &fnu, &kode, &NB, ysph, ysphi, &nz, auxbyr, auxbyi,
-	    &ierr);
-    cte = sqrt(0.5*PI/x);    
-    for(i = 0 ; i < NB ; i++ ) ysph[i] *= cte ;
-  }
-  
-  GetDP_End ;  
-}
-
-
-
-double AltSpherical_y_n(int n, double x){
-  /* Computes the Alternative Spherical Bessel function 'y' of order n and Real argument */
-  
-  double aysph, abyr, abyi, auxabyr, auxabyi, fnu, xi = 0. ;
-  int nz = 0, ierr, kode = 1, NB = 1 ;
-
-  GetDP_Begin("AltSpherical_y_n");
-
-  fnu = n+0.5;
-  zbesy_( &x, &xi, &fnu, &kode, &NB, &abyr, &abyi, &nz, &auxabyr, &auxabyi,
-	    &ierr);
-  aysph = sqrt(0.5*PI*x) * abyr;
-   
-  GetDP_Return(aysph);      
-}
-
-
 
 void PrintSpherical_j_n(int n, char * FileName){
 
@@ -185,6 +106,76 @@ void PrintSpherical_j_n(int n, char * FileName){
 }
 
 
+/* ------------------------------------------------------------------------ */
+/*  SPHERICAL BESSEL functions of the SECOND kind                           */
+/* ------------------------------------------------------------------------ */
+
+double Spherical_y_n(int n, double x){
+  /* Computes the Spherical Bessel function 'y' of order n and real
+     argument */
+  
+  double ysph, byr, byi, auxbyr, auxbyi, fnu, xi = 0. ;
+  int nz = 0,ierr, kode = 1, NB =1 ;
+
+  GetDP_Begin("Spherical_y_n");
+
+  if (x==0.) Msg(ERROR, "Singularity in Spherical_y_n");
+
+  fnu = n+0.5;
+  zbesy_(&x, &xi, &fnu, &kode, &NB, &byr, &byi, &nz, &auxbyr, &auxbyi, &ierr);
+  if(ierr) Msg(ERROR, "Exception caught in second kind Bessel function");
+
+  ysph = sqrt(0.5*PI/x) * byr;
+
+  GetDP_Return(ysph);   
+}
+
+void Spherical_y_nArray(int n, double x, int NB, double ysph[]){
+  /* Computes NB Spherical Bessel function 'y' Real argument x n is
+     the order of the first member of the sequence */
+
+  double cte, fnu, xi = 0., ysphi[NBR_MAX_EXP], auxbyr[NBR_MAX_EXP], auxbyi[NBR_MAX_EXP] ;
+  int i, nz = 0, ierr, kode = 1 ;
+
+  GetDP_Begin("Spherical_y_n_Array");
+
+  if (NB > NBR_MAX_EXP)
+    Msg(ERROR, "The number of Spherical_y_n required exceeds the maximum "
+	"defined: NBR_MAX_EXP = %d",NBR_MAX_EXP) ;
+
+  for(i = 0 ; i < NB ; i++ )
+    ysphi[i] = 0.; 
+
+  if (x == 0.){
+    ysph[0] = 1. ;
+  }
+  else{
+    fnu = n + 0.5 ; /* From order n to (n+NB-1) */
+    zbesy_(&x, &xi, &fnu, &kode, &NB, ysph, ysphi, &nz, auxbyr, auxbyi, &ierr);
+    if(ierr) Msg(ERROR, "Exception caught in second kind Bessel function");
+    cte = sqrt(0.5*PI/x);
+    for(i = 0 ; i < NB ; i++ ) ysph[i] *= cte ;
+  }
+  
+  GetDP_End ;  
+}
+
+double AltSpherical_y_n(int n, double x){
+  /* Computes the Alternative Spherical Bessel function 'y' of order n
+     and Real argument */
+  
+  double aysph, abyr, abyi, auxabyr, auxabyi, fnu, xi = 0. ;
+  int nz = 0, ierr, kode = 1, NB = 1 ;
+
+  GetDP_Begin("AltSpherical_y_n");
+
+  fnu = n+0.5;
+  zbesy_(&x, &xi, &fnu, &kode, &NB, &abyr, &abyi, &nz, &auxabyr, &auxabyi, &ierr);
+  if(ierr) Msg(ERROR, "Exception caught in second kind Bessel function");
+  aysph = sqrt(0.5*PI*x) * abyr;
+   
+  GetDP_Return(aysph);      
+}
 
 void PrintSpherical_y_n(int n, char * FileName){
 
@@ -208,12 +199,26 @@ void PrintSpherical_y_n(int n, char * FileName){
   GetDP_End;
 }
 
+/* ---------------------------------------------------------------------------------- */
+/*  SPHERICAL BESSEL functions of the THIRD kind  (First and second HANKEL functions) */
+/* ---------------------------------------------------------------------------------- */
 
+void Spherical_h_n(int type, int n, double x, double *hr, double *hi){
+  /* Computes the Spherical first or second Hankel function of order n
+     and Real argument */
+  
+  double fact, bhr, bhi, fnu, xi = 0. ;
+  int nz = 0, ierr, kode = 1, NB = 1 ;
 
+  GetDP_Begin("Spherical_h_n");
 
+  fnu = n + 0.5;
+  zbesh_(&x, &xi, &fnu, &kode, &type, &NB, &bhr, &bhi, &nz, &ierr) ;
+  if(ierr) Msg(ERROR, "Exception caught in third kind Bessel function");     
+  fact = sqrt(0.5*PI/x); 
+  *hr = fact * bhr;
+  *hi = fact * bhi;
 
-
-
-
-
+  GetDP_End;  
+}
 
--- Numeric/SphBessel.h
+++ Numeric/SphBessel.h
@@ -1,4 +1,4 @@
-/* $Id: SphBessel.h,v 1.2 2001-11-19 17:41:04 sabarieg Exp $ */
+/* $Id: SphBessel.h,v 1.3 2002-02-01 17:25:26 geuzaine Exp $ */
 
 #ifndef _SPHBESSEL_H_
 #define _SPHBESSEL_H_
@@ -13,13 +13,14 @@
 double Spherical_j_n(int n, double x) ;
 void Spherical_j_nArray(int n, double x, int NB, double jsph[]) ;
 double AltSpherical_j_n(int n, double x) ;
+void PrintSpherical_j_n(int n, char * FileName) ;
 
 double Spherical_y_n(int n, double x) ;
 void Spherical_y_nArray(int n, double x, int NB, double ysph[]) ;
 double AltSpherical_y_n(int n, double x) ;
-
-void PrintSpherical_j_n(int n, char * FileName) ;
 void PrintSpherical_y_n(int n, char * FileName) ;
+
+void Spherical_h_n(int type, int n, double x, double *hr, double *hi);
 
 
 #endif
