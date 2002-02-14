5e7bb66da4c40e6d1aa2f01f6fa70fda1186bd02
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2002-02-14 00:59:46 +0000

    This commit was manufactured by cvs2svn to create tag 'stable-mail-reitich'.

--- Integration/NewtonCotes_Quadrangle.c
+++
@@ -1,7 +0,0 @@
-#define RCSID "$Id: NewtonCotes_Quadrangle.c,v 1.4 2000-10-30 01:29:47 geuzaine Exp $"
-
-/* NewtonCotes integration over a quadrangle */
-
-void  NewtonCotes_Quadrangle (){
-  return ;
-}
