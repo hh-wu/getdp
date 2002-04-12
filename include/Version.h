eeb88f039da9cf190c6da7fd085e8e6f98e0609f
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2002-04-12 22:36:31 +0000

    This commit was manufactured by cvs2svn to create tag 'scat-oscar+andy-v2'.

--- include/Version.h
+++
@@ -1,5 +0,0 @@
-#define GETDP_VERSION  0.84
-#define GETDP_DATE     "Thu Nov 22 14:52:41 CET 2001"
-#define GETDP_HOST     "elap83"
-#define GETDP_PACKAGER "dular"
-#define GETDP_OS       "Linux 2.2.16-22"
