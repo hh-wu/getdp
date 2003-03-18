a5c397d841e81a12b4b4e24b15dee57a01cafee5
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2003-03-18 15:39:17 +0000

    More header cleanup

--- Main/Pre_GetDofFMMGroup.h
+++ Main/Pre_GetDofFMMGroup.h
@@ -1,12 +1,8 @@
-/* $Id: Pre_GetDofFMMGroup.h,v 1.1 2003-03-17 10:50:32 sabarieg Exp $ */
-#ifndef _PRE_GETDOFFMMGROUP
-#define _PRE_GETDOFFMMGROUP
+/* $Id: Pre_GetDofFMMGroup.h,v 1.2 2003-03-18 15:39:17 geuzaine Exp $ */
+#ifndef _PRE_GET_DOF_FMM_GROUP_H_
+#define _PRE_GET_DOF_FMM_GROUP_H_
 
-#include <stdio.h>
-#include <math.h>
-
-#include "GetDP.h"
-#include "Data_FMM.h"
+#include "Data_Active.h"
 
 void  Pre_GetDofFMMGroup(struct Formulation * Formulation_P,
 			 struct QuantityStorage * QuantityStorage_P0) ;  
