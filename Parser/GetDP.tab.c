c66e11939177dc59f76beef95758de4c8b5ae530
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2008-04-28 15:35:24 +0000

    *** empty log message ***

--- Parser/GetDP.tab.c
+++ Parser/GetDP.tab.c
@@ -580,7 +580,7 @@
 /* Copy the first part of user declarations.  */
 #line 1 "GetDP.y"
 
-/* $Id: GetDP.tab.c,v 1.132 2008-04-24 21:10:57 dular Exp $ */
+/* $Id: GetDP.tab.c,v 1.133 2008-04-28 15:35:21 geuzaine Exp $ */
 /*
  * Copyright (C) 1997-2006 P. Dular, C. Geuzaine
  *
@@ -788,7 +788,7 @@ typedef union YYSTYPE
   List_T  *l ;
   struct TwoInt t ;
 }
-/* Line 187 of yacc.c.  */
+/* Line 193 of yacc.c.  */
 #line 793 "GetDP.tab.c"
 	YYSTYPE;
 # define yystype YYSTYPE /* obsolescent; will be withdrawn */
@@ -852,7 +852,7 @@ typedef short int yytype_int16;
 #define YYSIZE_MAXIMUM ((YYSIZE_T) -1)
 
 #ifndef YY_
-# if YYENABLE_NLS
+# if defined YYENABLE_NLS && YYENABLE_NLS
 #  if ENABLE_NLS
 #   include <libintl.h> /* INFRINGES ON USER NAME SPACE */
 #   define YY_(msgid) dgettext ("bison-runtime", msgid)
@@ -4270,7 +4270,7 @@ while (YYID (0))
    we won't break user code: when these are the locations we know.  */
 
 #ifndef YY_LOCATION_PRINT
-# if YYLTYPE_IS_TRIVIAL
+# if defined YYLTYPE_IS_TRIVIAL && YYLTYPE_IS_TRIVIAL
 #  define YY_LOCATION_PRINT(File, Loc)			\
      fprintf (File, "%d.%d-%d.%d",			\
 	      (Loc).first_line, (Loc).first_column,	\
