12ea4ce80c2db2b6c032ad916c8526cee8d94c70
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2008-06-11 10:19:00 +0000

    fix compile

--- Function/F_Raytracing.c
+++ Function/F_Raytracing.c
@@ -1,3 +1,27 @@
+#define RCSID "$Id: F_Raytracing.c,v 1.6 2008-06-11 10:18:59 geuzaine Exp $"
+/*
+ * Copyright (C) 1997-2006 P. Dular, C. Geuzaine
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA.
+ *
+ * Please report all bugs and problems to <getdp@geuz.org>.
+ *
+ * Contributor(s):
+ */
+
 #include "GetDP.h"
 #include "Data_DefineE.h"
 #include "F_Function.h"
@@ -184,7 +208,7 @@ void F_DiamondPhase(F_ARG)
 {
   double x, y, phase, r, theta, xtrans, ytrans;
 
-  GetDP_Begin(F_DiamondPhase);
+  GetDP_Begin("F_DiamondPhase");
 
   x = A->Val[0];
   y = A->Val[1];
