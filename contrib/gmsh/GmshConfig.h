9a6b0151f086c56c9d47d119eec07f1c9c2f44f3
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2009-03-29 10:23:20 +0000

    *** empty log message ***

---
+++ contrib/gmsh/GmshConfig.h
@@ -0,0 +1,41 @@
+/* Common/GmshConfig.h.  Generated from GmshConfig.h.in by configure.  */
+// Gmsh - Copyright (C) 1997-2009 C. Geuzaine, J.-F. Remacle
+//
+// See the LICENSE.txt file for license information. Please report all
+// bugs and problems to <gmsh@geuz.org>.
+
+#ifndef _GMSH_CONFIG_H_
+#define _GMSH_CONFIG_H_
+
+/* #undef HAVE_64BIT_SIZE_T */
+#define HAVE_ANN 1
+#define HAVE_BLAS 1
+#define HAVE_CHACO 1
+#define HAVE_FLTK 1
+#define HAVE_FOURIER_MODEL 1
+#define HAVE_GMM 1
+#define HAVE_LAPACK 1
+/* #undef HAVE_LIBCGNS */
+#define HAVE_LIBJPEG 1
+#define HAVE_LIBPNG 1
+#define HAVE_LIBZ 1
+#define HAVE_MATH_EVAL 1
+#define HAVE_MED 1
+#define HAVE_METIS 1
+/* #undef HAVE_MPI */
+#define HAVE_NATIVE_FILE_CHOOSER 1
+#define HAVE_NETGEN 1
+#define HAVE_NO_DLL 1
+/* #undef HAVE_NO_PARSER */
+/* #undef HAVE_NO_POST */
+/* #undef HAVE_NO_SOCKLEN_T */
+/* #undef HAVE_NO_VSNPRINTF */
+#define HAVE_OCC 1
+/* #undef HAVE_OCC_MESH_CONSTRAINTS */
+/* #undef HAVE_OSMESA */
+#define HAVE_TETGEN 1
+#define HAVE_TREE_BROWSER 1
+
+#define GMSH_CONFIG_OPTIONS " Fltk NativeFileChooser TreeBrowser Jpeg Png Ann Gmm Chaco Metis Netgen Tetgen MathEval Occ Med Libz Blas Lapack FourierModel NoDll"
+
+#endif
--- contrib/gmsh/GmshVersion.h
+++ contrib/gmsh/GmshVersion.h
@@ -3,7 +3,7 @@
 #define GMSH_PATCH_VERSION 1
 #define GMSH_EXTRA_VERSION ""
 #define GMSH_VERSION       "2.3.1"
-#define GMSH_DATE          "Sun Mar 22 16:24:16 CET 2009"
+#define GMSH_DATE          "Wed Mar 25 20:16:10 CET 2009"
 #define GMSH_HOST          "mbp-2.local"
 #define GMSH_PACKAGER      "geuzaine"
 #define GMSH_OS            "Darwin"
