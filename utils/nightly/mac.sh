01b6de6e0eaf662563cf7a5d545b71aff5b2d44a
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2009-08-18 08:21:41 +0000

    update nightly procedures

---
+++ utils/nightly/mac.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+GETDP=${HOME}/src/getdp
+LOG=${GETDP}/nightly.log
+WEB_BIN=geuzaine@geuz.org:/home/www/geuz.org/getdp/bin/MacOSX
+
+rm -f ${LOG}
+rm -f ${GETDP}/Makefile*
+rm -rf ${GETDP}/getdp-*cvs*
+echo "BUILD BEGIN: `date`" > ${LOG}
+cd ${GETDP} && export CVS_RSH=ssh && cvs update -dPA >> ${LOG} 2>&1
+cd ${GETDP} && ./configure\
+  --enable-petsc\
+  --with-petsc-prefix=${HOME}/src/petsc-3.0.0-p7\
+  --with-petsc-arch=umfpack-cxx-opt\
+  --with-gsl-prefix=/usr/local >> ${LOG} 2>&1
+cd ${GETDP} && make clean >> ${LOG} 2>&1
+cd ${GETDP} && make distrib-mac-nightly >> ${LOG} 2>&1
+echo "BUILD END: `date`" >> ${LOG}
+scp -C ${GETDP}/getdp-*cvs*.tgz ${WEB_BIN}/MacOSX/getdp-nightly-MacOSX.tgz
+scp -C ${LOG} ${WEB_BIN}/MacOSX/
---
+++ utils/nightly/windows.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+GETDP=${HOME}/src/getdp
+LOG=${GETDP}/nightly.log
+WEB_BIN=geuzaine@geuz.org:/home/www/geuz.org/getdp/bin/Windows
+
+rm -f ${LOG}
+rm -f ${GETDP}/Makefile*
+rm -rf ${GETDP}/getdp-*cvs*
+echo "BUILD BEGIN: `date`" > ${LOG}
+cd ${GETDP} && export CVS_RSH=ssh && cvs update -dPA >> ${LOG} 2>&1
+cd ${GETDP} && ./configure\
+  --enable-petsc\
+  --with-petsc-prefix=${HOME}/src/petsc-3.0.0-p7\
+  --with-petsc-arch=umfpack-cxx-opt\
+  --with-gsl-prefix=/usr/local >> ${LOG} 2>&1
+cd ${GETDP} && make clean >> ${LOG} 2>&1
+cd ${GETDP} && make distrib-win-nightly >> ${LOG} 2>&1
+echo "BUILD END: `date`" >> ${LOG}
+scp -C ${GETDP}/getdp-*cvs*.zip ${WEB_BIN}/Windows/getdp-nightly-Windows.zip
+scp -C ${LOG} ${WEB_BIN}/Windows/
