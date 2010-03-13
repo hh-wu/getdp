aa64ff7bc59086f7a35efbaa5f209d9ce3f2e5e6
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2010-03-13 13:28:57 +0000

    prealloc full lines

--- Legacy/LinAlg_PETSC.cpp
+++ Legacy/LinAlg_PETSC.cpp
@@ -122,10 +122,10 @@ void LinAlg_CreateMatrix(gMatrix *M, gSolver *Solver, int n, int m)
   PetscOptionsGetInt(PETSC_NULL, "-petsc_prealloc", &prealloc, &set);
   std::vector<int> nnz(n, prealloc);
   
-  // Huge hack: we preallocate the last 20 lines with more nnz to deal
-  // speed-up assembly time when global quantities exist (global
+  // Huge hack: we preallocate the last 10 lines of the matrix as full
+  // to speed-up assembly time when global quantities exist (global
   // quantities are numbered last) ;-)
-  for(int i = n - 20; i < n; i++) nnz[i] = n / 2;
+  for(int i = n - 10; i < n; i++) nnz[i] = n;
 
   ierr = MatCreateSeqAIJ(PETSC_COMM_WORLD, n, m, 0, &nnz[0], &M->M);
 
