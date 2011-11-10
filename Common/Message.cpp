e02c9658517860dbb4af44cfc3f5d0cf7d9e7365
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2011-11-10 07:13:14 +0000

    onelab

--- Common/Message.cpp
+++ Common/Message.cpp
@@ -436,10 +436,10 @@ void Message::ExchangeOnelabParameter(Constant *c,
         o.setMin(fopt["Min"][0]); o.setMax(fopt["Max"][0]);
       }
       else if(fopt.count("Min")){
-        o.setMin(fopt["Min"][0]); o.setMax(1.e200);
+        o.setMin(fopt["Min"][0]); o.setMax(onelab::parameter::maxNumber());
       }
       else if(fopt.count("Max")){
-        o.setMax(fopt["Max"][0]); o.setMin(-1.e200);
+        o.setMax(fopt["Max"][0]); o.setMin(-onelab::parameter::maxNumber());
       }
       if(fopt.count("Step")) o.setStep(fopt["Step"][0]);
       if(fopt.count("Choices")) o.setChoices(fopt["Choices"]);
