9fcdc14555a2b7417b0f6971db923be02274fe67
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2012-02-06 12:40:45 +0000

    example for Bert

--- Legacy/IterativeLinearSolver.cpp
+++ Legacy/IterativeLinearSolver.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "ProData.h"
+#include "SolvingOperations.h"
 
 void Operation_IterativeLinearSolver(struct Resolution  *Resolution_P,
                                      struct Operation   *Operation_P,
@@ -8,5 +9,12 @@ void Operation_IterativeLinearSolver(struct Resolution  *Resolution_P,
 {
 
   printf("HELLO! %s\n", Operation_P->Case.IterativeLinearSolver.Type);
+
+  for(int i = 0; i < 10; i++){
+    printf("Performing GetDP operations for Ax -- ITER %d...\n", i);
+    Treatment_Operation(Resolution_P,
+                        Operation_P->Case.IterativeLinearSolver.Operations_Ax,
+                        DofData_P0, GeoData_P0, NULL, NULL) ;
+  }
 
 }
