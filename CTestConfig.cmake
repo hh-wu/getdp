d8ad3856dab2d9084c4b2385e28ab44621586f7f
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2012-08-16 09:42:53 +0000

    allow passing PETSC_DIR and PETSC_ARCH directly to cmake

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,3 +1,10 @@
+# GetDP - Copyright (C) 1997-2012 P. Dular, C. Geuzaine
+#
+# See the LICENSE.txt file for license information. Please report all
+# bugs and problems to <getdp@geuz.org>.
+
+cmake_minimum_required(VERSION 2.6 FATAL_ERROR)
+
 set(CTEST_PROJECT_NAME "GetDP")
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE_CDASH TRUE)
