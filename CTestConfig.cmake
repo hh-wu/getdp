2b9c506f4f64e18e420a8a809ebd69a00876389a
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2012-08-16 10:03:27 +0000

    requite cmake 2.8

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -3,8 +3,6 @@
 # See the LICENSE.txt file for license information. Please report all
 # bugs and problems to <getdp@geuz.org>.
 
-cmake_minimum_required(VERSION 2.6 FATAL_ERROR)
-
 set(CTEST_PROJECT_NAME "GetDP")
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE_CDASH TRUE)
