e39a127a36ac344f5b994a1e59aacb4dd8cf38f2
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2014-01-30 13:50:25 +0000

    up

--- benchmarks/machines/driver.cpp
+++ benchmarks/machines/driver.cpp
@@ -67,7 +67,8 @@ int main(int argc, char **argv)
     c->runSubClient("Gmsh", std::string("gmsh ") + machine + ".geo -2 -v 2");
 
     // run getdp as a subclient
-    c->runSubClient("GetDP", std::string("getdp ") + machine + " -msh " +
+    c->runSubClient("GetDP", std::string("getdp ") + machine +
+                    " -setnumber Flag_PrintFields 0 -msh " +
                     machine + ".msh" + " -solve Analysis -v 2");
 
     // retrieve the torque
