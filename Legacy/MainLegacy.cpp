7adcdc14b76d90af714c1589d9713bc935f32e6e
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2014-08-16 06:17:43 +0000

    update copyright notice

--- Legacy/MainLegacy.cpp
+++ Legacy/MainLegacy.cpp
@@ -1,4 +1,4 @@
-// GetDP - Copyright (C) 1997-2014 P. Dular, C. Geuzaine
+// GetDP - Copyright (C) 1997-2014 P. Dular and C. Geuzaine, University of Liege
 //
 // See the LICENSE.txt file for license information. Please report all
 // bugs and problems to the public mailing list <getdp@geuz.org>.
@@ -41,7 +41,7 @@ static void Info(int level, char *arg0)
   case 0 :
     fprintf(stderr,
 	    "GetDP, a General environment for the treatment of Discrete Problems\n"
-	    "Copyright (C) 1997-2014 P. Dular, C. Geuzaine\n"
+	    "Copyright (C) 1997-2014 P. Dular and C. Geuzaine, University of Liege\n"
 	    "Usage: %s [file] [options]\n"
 	    "Processing options:\n"
 	    "  -pre 'Resolution'         pre-processing\n"
--- README.txt
+++ README.txt
@@ -1,7 +1,8 @@
 This is GetDP, a General environment for the treatment of Discrete Problems.
 
-GetDP is distributed under the terms of the GNU General Public License, Version
-2 or later. See doc/LICENSE.txt and doc/CREDITS.txt for more information.
+GetDP is copyright (C) 1997-2014 P. Dular and C. Geuzaine, University of Liege,
+and is distributed under the terms of the GNU General Public License, Version 2
+or later. See doc/LICENSE.txt and doc/CREDITS.txt for more information.
 
 See the doc/ and demos/ directories for documentation. The reference manual is
 located in doc/texinfo/. See the web site http://geuz.org/getdp for additional
