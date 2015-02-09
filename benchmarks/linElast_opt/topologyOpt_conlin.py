fb5ca6bae5ce9e4b556af6b2d365986e006510e6
Erin Kuci <erin.kuci@ulg.ac.be>
2015-02-09 08:52:51 +0000

    Deleted file.

--- benchmarks/linElast_opt/topologyOpt_conlin.py
+++ benchmarks/linElast_opt/topologyOpt_conlin.py
@@ -4,75 +4,61 @@
     Topology optimization using SIMP method and MMA algorithm (matlab)
     
 """
-
 import sys
 sys.path.insert(0,'/Users/erinkuci/Desktop/src/getdp/benchmarks_kst/tool')
-from tool4 import *
+from tool import *
 
 # ************************************************************************
 # ***** Create the parameters                                        *****
 # ************************************************************************
-parameters = Dictionnaire()
+parameters = Dictionnary()
 
 parameters['warmStart'] = 0
+parameters['plot'] = 1
 
 # Model
-parameters['modelName'] = 'def'
+parameters['fileName'] = 'def'
 parameters['AnalysisModelType']='FEM'
-parameters['flagParallel'] = 0
-parameters['flagOptType'] = 'Topology' #'Shape', 'Topology'
-parameters['modelType'] = 'beam'
+parameters['flagOptType'] = 1 #0:'Shape',1:'Topology'
+parameters['defautValue']={'MaterialInterpLaw':0,'SimpDegree':3.0,
+                           'RecombineSurf':1,'lc':2.0}
 
 # Design variables
 parameters['paramNameDisp'] = 'E'
 parameters['VolFrac'] = 0.4
+parameters['elementOfDomainTopOptTAG'] = [1000]
+x0 = np.array([parameters['VolFrac']])
+xmax = np.array([1.0])
+xmin = np.array([0.001])
 
 # Performance function
 parameters['performance'] = ['Compliance', 'Volume']
 parameters['m'] = len(parameters['performance']) - 1 # number of constraint
-parameters['fiMax'] = np.zeros(parameters['m']+1)
-parameters['normalizeObj'] = 0
+parameters['sign'] = [1.0,1.0]
 
 # Sensitivity analysis
 parameters['flag_computeGrad'] = 1
 parameters['SensitivityMethod'] = ['AnalyticAvmFixedDom','Analytic']
-parameters['FilterSensitivity'] = [1, 0]
-parameters['rmin'] = 0.007*2.5
+parameters['FilterSensitivity'] = [1,0]
+parameters['rmin'] = 0.009
 
 # Optimizer set-up
-parameters['optimizer']='conlin'
-parameters['solverName'] = 'CONLIN'
+parameters['optimizer']= 'conlinFile' #'conlinFile','gcmma'
+parameters['solverName'] = 'CONLIN' #'OC','CONLIN','GCMMA-SVANBERG'
 parameters['xtol'] = 1.0e-02
-
 # ************************************************************************
 # ***** Instantiate the Model and the Optimizer                      *****
 # ************************************************************************
-op = OPTIMIZATION(parameters)
+op = Optimization(parameters,xmin,xmax,x0)
 
 # ************************************************************************
 # ***** Optimization routine                                         *****
 # ************************************************************************
 # Preprocess
-op.parameters['fiMax'][1] = op.parameters['VolFrac']*op.parameters['n']
-op.parameters['VolMax'] =  op.parameters['n']
-if(not(parameters['warmStart'])):
-    op.x0 = np.array([op.parameters['VolFrac']]*op.parameters['n'])
-op.xmax = np.array([1.0]*op.parameters['n'])
-op.xmin = np.array([0.001]*op.parameters['n'])
-
 op.preprocessing(op.parameters)
 
-# Create optimizer
-op.create(op.parameters)
-
 # Call optimizer
-dc = np.zeros(op.parameters['m']+1)
-jt = np.array([4.0,4.0])
-#d2 = np.zeros((op.parameters['m']+1)*op.parameters['n'])
-d2 = np.array([0.01]*(op.parameters['m']+1)*op.parameters['n'])
-pds = 0.0
-op.conlinFor(x0,xmax,xmin,op.parameters['fiMax'],
-             dc,jt,pds,d2,op.parameters)
+op.solveOpt(op.x0,op.xmax,op.xmin,op.fjMax,2,op.parameters)
 
 # Close optimizer
 op.close()
@@ -81,21 +67,9 @@ op.close()
 # ***** Optimization Post-Process                                    *****
 # ************************************************************************
 # Optimization history
-x0,xf,obj,constr = op.postprocessing(x0,parameters)
+#x,f = op.postprocessing(op.parameters)
 
-#os.system('cp '+'res/designVar' + str(iterMax)+'.pos '
-#               +'res/designVarFilt.pos')
-#
-#x = op.readPostOpFile(n=op.n, path='res/designVarFilt.pos')
-#xf = x[:]
-#for k in range(op.n):
-#    if(x[k] <= 0.8):
-#        xf[k] = 0.0
-#    else:
-#        xf[k] = 1.0
-#op.refreshPostOpFile(x=xf, path='res/designVarFilt.pos',
-#                     indexStart=op.parameters['indexStart'])
-#
+
 
 
 
