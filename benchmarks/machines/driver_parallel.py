cd4e3d5f38e89251b200fc1da422f2343a244ded
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2015-03-08 12:06:41 +0000

    client names should be unique

--- benchmarks/machines/driver_parallel.py
+++ benchmarks/machines/driver_parallel.py
@@ -28,7 +28,7 @@ for angle in angles:
    msh = machine + '_' + tag + '.msh'
    c.setNumber(tag + '/Input/21Start rotor angle [deg]', value=angle)
    # run gmsh as a non-blocking subclient
-   c.runNonBlockingSubClient('myGmsh', 'gmsh -setstring ResId ' + tag + '/ ' + 
+   c.runNonBlockingSubClient('myGmsh' + tag, 'gmsh -setstring ResId ' + tag + '/ ' + 
                              machine + '.geo -2 -v 2 -o ' + msh)
    
 c.waitOnSubClients()
@@ -38,7 +38,7 @@ for angle in angles:
    tag = 'angle' + str(angle)
    msh = machine + '_' + tag + '.msh'
    # run getdp as a non-blocking subclient
-   c.runNonBlockingSubClient('myGetDP', 'getdp -setstring ResId ' + tag + '/ ' + 
+   c.runNonBlockingSubClient('myGetDP' + tag, 'getdp -setstring ResId ' + tag + '/ ' + 
                              '-setnumber Flag_PrintFields 0 ' +
                              machine + ' -msh ' + msh + ' -solve Analysis -v 2')
 
