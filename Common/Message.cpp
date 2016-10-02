51fa1dc92d6f9e2810b64fffe192883de3baa5c6
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2016-10-02 20:54:13 +0000

    fix Py_SetProgramName on python 3.5

--- Common/Message.cpp
+++ Common/Message.cpp
@@ -126,7 +126,7 @@ void Message::Initialize(int argc, char **argv)
   std::vector<wchar_t*> wargv(argc ? argc : 1);
   for(int i = 0; i < argc; i++)
     wargv[i] = Py_DecodeLocale(argv[i], NULL);
-  Py_SetProgramName(wargv[0], NULL);
+  Py_SetProgramName(wargv[0]);
   Py_InitializeEx(0);
   PySys_SetArgv(argc, &wargv[0]);
 #endif
