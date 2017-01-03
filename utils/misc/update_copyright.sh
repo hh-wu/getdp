0c54adce970bc3869ac4a7b6ceab7ac2105a0f5a
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2017-01-03 14:51:09 +0000

    happy new year!

--- utils/misc/update_copyright.sh
+++ utils/misc/update_copyright.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -
 
-files=`find ../.. -not -path "*.svn*" -and -not -path "*lib*" -and -not -path "*bin*" -and -not -path "*build_*" -and -not -name "update_copyright.sh" | xargs grep 'Copyright (C) 1997-2015 P. Dular and C. Geuzaine' -sl`
+files=`find ../.. -not -path "*.svn*" -and -not -path "*lib*" -and -not -path "*bin*" -and -not -path "*build_*" -and -not -name "update_copyright.sh" | xargs grep 'Copyright (C) 1997-2016 P. Dular and C. Geuzaine' -sl`
 
 echo $files
 
 for file in $files 
 do
-sed "s|(C) 1997-2015 P\. Dular and C\. Geuzaine|(C) 1997-2016 P\. Dular and C\. Geuzaine|g" $file > $file.tmp
+sed "s|(C) 1997-2016 P\. Dular and C\. Geuzaine|(C) 1997-2017 P\. Dular and C\. Geuzaine|g" $file > $file.tmp
 echo modified $file
 rm -f $file
 mv $file.tmp $file
@@ -16,4 +16,5 @@ done
 #   README.txt
 #   doc/CREDITS.txt
 #   doc/texinfo/getdp.texi
+#   doc/getdp.html
 
