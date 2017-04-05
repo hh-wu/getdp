e1681fda818d60f30d4d8cc0bab43240b3ad5a06
Guillaume Demesy <guillaume.demesy@fresnel.fr>
2017-04-05 19:00:41 +0200

    PETSC_DEFAULT

--- Kernel/EigenSolve_SLEPC.cpp
+++ Kernel/EigenSolve_SLEPC.cpp
@@ -824,7 +824,7 @@ static void _nonlinearEVP(struct DofData * DofData_P, int numEigenValues,
   // SAME_NONZERO_PATTERN
   _try(NEPSetSplitOperator(nep,2,A,funs,SUBSET_NONZERO_PATTERN));
   _try(NEPSetDimensions(nep, numEigenValues, PETSC_DECIDE, PETSC_DECIDE));
-  _try(NEPSetTolerances(nep, 1.e-6, 100));
+  _try(NEPSetTolerances(nep, 1.e-6, PETSC_DEFAULT));
   _try(NEPSetType(nep, NEPNLEIGS));
   _try(NEPSetWhichEigenpairs(nep, NEP_LARGEST_MAGNITUDE));
   _try(NEPMonitorSet(nep, _myNepMonitor, PETSC_NULL, PETSC_NULL));
