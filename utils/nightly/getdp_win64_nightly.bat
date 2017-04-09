a5f880388ed23dab4897d95745db674b26f12c70
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2017-04-09 23:12:09 +0200

    updated win64 build machine

--- utils/nightly/getdp_win64_nightly.bat
+++ utils/nightly/getdp_win64_nightly.bat
@@ -1,6 +1,6 @@
 @echo off
 
 C:
-chdir C:\cygwin\bin
+chdir C:\cygwin64\bin
 
 bash --login -c "cd ${HOME}/src/getdp/utils/nightly && ctest -S getdp_win64_nightly.ctest -VV > ${HOME}/src/getdp/nightly.log 2>&1"
