5a8dae83134b1b080ab99c832ba30ba5cf91d24f
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2017-06-15 14:07:43 +0200

    up

--- utils/nightly/getdp_win64_nightly.bat
+++ utils/nightly/getdp_win64_nightly.bat
@@ -3,4 +3,6 @@
 C:
 chdir C:\cygwin64\bin
 
+bash --login -c "cd ${HOME}/src/getdp/contrib/NX && git pull 2>&1"
+
 bash --login -c "cd ${HOME}/src/getdp/utils/nightly && ctest -S getdp_win64_nightly.ctest -VV > ${HOME}/src/getdp/nightly.log 2>&1"
