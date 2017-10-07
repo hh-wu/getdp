245d5a3c39cf69e9ecb10894addb69daea6ce0e9
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2017-10-07 13:33:13 +0200

    moved automatic build infrastructure to gitlab

--- utils/nightly/getdp_mac_nightly.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# this is supposed to be called by launchd:
-#  * copy org.geuz.getdp.nightly.plist to ~/Library/LauchAgents/
-#  * launchctl load ~/Library/LaunchAgents/org.geuz.getdp.nightly.plist
-
-cd ${HOME}/src/getdp/utils/nightly && /usr/local/bin/ctest -S getdp_mac_nightly.ctest -VV > ${HOME}/src/getdp/nightly.log 2>&1
