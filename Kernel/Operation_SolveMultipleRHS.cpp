46c35b1d00dd5d3d64dcf80ea4c77b76e45ebc94
Christophe Geuzaine <cgeuzaine@ulg.ac.be>
2018-08-31 15:29:33 +0200

    missing

---
+++ Kernel/Operation_SolveMultipleRHS.cpp
@@ -0,0 +1,18 @@
+// GetDP - Copyright (C) 1997-2018 P. Dular and C. Geuzaine, University of Liege
+//
+// See the LICENSE.txt file for license information. Please report all
+// bugs and problems to the public mailing list <getdp@onelab.info>.
+//
+// Contributed by Erin Kuci
+
+#include "DofData.h"
+#include "Message.h"
+#include "GetDPConfig.h"
+
+void Operation_SolveMultipleRHS(struct DofData *DofData_P)
+{
+  Message::Info("TODO Erin !!");
+#if defined(HAVE_PETSC)
+  // bla
+#endif
+}
