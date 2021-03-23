1566b80a34d3a0aa12273921575947dfd84591f3
Christophe Geuzaine <cgeuzaine@uliege.be>
2021-03-23 17:08:05 +0100

    use wmain() on windows to handle non-ASCII command line arguments; patch packported from Gmsh - should fix #74)

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -1,4 +1,4 @@
-// GetDP - Copyright (C) 1997-2020 P. Dular and C. Geuzaine, University of Liege
+// GetDP - Copyright (C) 1997-2021 P. Dular and C. Geuzaine, University of Liege
 //
 // See the LICENSE.txt file for license information. Please report all
 // issues on https://gitlab.onelab.info/getdp/getdp/issues.
@@ -10,8 +10,44 @@
 #include "ProData.h"
 #include "getdp.h"
 
+#if defined(WIN32) && !defined(__CYGWIN__)
+
+// in order to handle non-ASCII command line arguments on Windows, use wmain()
+// instead of main() (we could also use main() and retrieve the "wide" args with
+// GetCommandLineW() later on, but this would have side-effects on the flow);
+// using wmain() with the mingw compilers requires adding the "-municode" linker
+// flag
+
+#include <windows.h>
+#include <wchar.h>
+
+static char *toUTF8(wchar_t *src)
+{
+  if(!src) return nullptr;
+  size_t srclen = wcslen(src);
+  int len = WideCharToMultiByte(CP_UTF8, 0, src, srclen, 0, 0, nullptr, nullptr);
+  char *out = new char[len + 1];
+  if(out) {
+    WideCharToMultiByte(CP_UTF8, 0, src, srclen, out, len, nullptr, nullptr);
+    out[len] = '\0';
+  }
+  return out;
+}
+
+int wmain(int argc, wchar_t *wargv[], wchar_t *envp[])
+{
+  char **argv = new char*[argc + 1];
+  for(int i = 0; i < argc; i++)
+    argv[i] = toUTF8(wargv[i]);
+  argv[argc] = nullptr;
+
+#else
+
 int main(int argc, char **argv)
 {
+
+#endif
+
 #if defined(HAVE_KERNEL)
   Message::SetExitOnError(1);
   MainKernel(argc, argv);
